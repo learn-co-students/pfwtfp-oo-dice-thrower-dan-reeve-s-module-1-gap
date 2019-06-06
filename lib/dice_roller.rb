7c691d285223116eb0da728d24d454f7d89ee5fd
Steven G. Harms <steven.harms@flatironschool.com>
2019-06-06 13:33:53 -0400

    Solution

--- lib/dice_roller.rb
+++ lib/dice_roller.rb
@@ -1,1 +1,15 @@
-# Your code here
+class DiceRoller
+  def initialize(die_count=1, max_pips_per_die=6)
+    @die_count = die_count
+    @max_pips_per_die = max_pips_per_die
+    @set_of_dice = generate_set
+  end
+
+  def generate_set
+    Array.new(@die_count){ Die.new(@max_pips_per_die) }
+  end
+
+  def dice
+    @set_of_dice
+  end
+end
--- lib/dice_thrower.rb
+++ lib/dice_thrower.rb
@@ -1,1 +1,16 @@
-# Your code here
+
+class Die
+  attr_reader :max_pips
+
+  def initialize(max_pips=6)
+    @max_pips = max_pips
+  end
+
+  def roll
+    rand(max_pips) + 1
+  end
+
+  def pips
+    max_pips
+  end
+end
